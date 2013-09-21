365b9fdc417a613a416c12f31ef4b281a5411772
Michael de Silva <michael@mwdesilva.com>
2013-09-22 01:08:27 +0530

    Fix bad indentation

--- go_json_example.go
+++ go_json_example.go
@@ -1,7 +1,7 @@
 /*
- * Michael de Silva <michael@mwdesilva.com>
- * CEO @ http://omakaselabs.com/ / https://github.com/bsodmike
- */
+* Michael de Silva <michael@mwdesilva.com>
+* CEO @ http://omakaselabs.com/ / https://github.com/bsodmike
+*/
 
 package main
 
@@ -17,29 +17,29 @@ func getContent(url string) ([]byte, error) {
     return nil, err
   }
 
-	// Send the request via a client
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
+  // Send the request via a client
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+    return nil, err
+  }
 
-	// Defer the closing of the body
-	defer resp.Body.Close()
+  // Defer the closing of the body
+  defer resp.Body.Close()
 
-	// Read the content into a byte array
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
+  // Read the content into a byte array
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return nil, err
+  }
 
-	// At this point we're done - simply return the bytes
-	return body, nil
+  // At this point we're done - simply return the bytes
+  return body, nil
 }
 
 func main() {
   result, _ := getContent("http://google.com")
-	fmt.Printf("Output\n")
+  fmt.Printf("Output\n")
   fmt.Printf(string(result))
 }
 
